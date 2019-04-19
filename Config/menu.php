be35c5c6d149a2ec5188c24f13368baf543de435
SyntesyDigital <ndelcast.dev@gmail.com>
2019-04-19 19:11:51 +0200

    Remove unused modules menu reference

--- Config/menu.php
+++ Config/menu.php
@@ -11,38 +11,6 @@ return [
     ],
 
     [
-        "route" => 'rrhh.admin.offers.index',
-        "label" => 'rrhh::header.offers',
-        "patterns" => [
-            'architect/offers*',
-            'architect/tags*'
-        ],
-        "roles" => []
-    ],
-
-    /*
-    [
-        'group' => true,
-        'name' => 'architect::plugins.topbar.menu'
-    ],
-
-    [
-        "route" => 'rrhh.admin.candidates.index',
-        "label" => 'rrhh::header.candidates',
-        "patterns" => [
-            'architect/candidates*'
-        ],
-        "roles" => ['admin']
-    ],
-    [
-        "route" => 'rrhh.admin.customers.index',
-        "label" => 'rrhh::header.customers',
-        "patterns" => [
-            'architect/customers*'
-        ],
-        "roles" => ['admin']
-    ],    */
-    [
         "route" => 'contents',
         "label" => 'architect::header.contents',
         "patterns" => [
@@ -50,16 +18,8 @@ return [
         ],
         "roles" => ['admin']
     ],
-    [
-        "route" => 'documents.index',
-        "label" => 'documents::header.documents',
-        "patterns" => [
-            'architect/documents*'
-        ],
-        "roles" => ['admin']
-    ],
 
-  /*  [
+    [
         "route" => 'medias.index',
         "label" => 'architect::header.media',
         "patterns" => [
@@ -67,7 +27,7 @@ return [
         ],
         "roles" => ['admin']
     ],
-*/
+
     [
         "route" => 'settings',
         "label" => 'architect::header.configuration',
