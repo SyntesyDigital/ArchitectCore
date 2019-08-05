8fb0446a65895092189c051d9c8437db69f1c8b2
DANI <dani@example.com>
2019-08-05 14:02:11 +0200

    add Helper for geting sitelists options

--- Helpers/siteListValue.php
+++ Helpers/siteListValue.php
@@ -17,4 +17,19 @@ if (!function_exists('getSiteListValue')) {
 
       return isset($values[$option])?$values[$option]:false ;
     }
+
+    function getSiteListValuesArray($identifier) {
+
+      $list = SiteList::where('identifier', $identifier)->first();
+
+      if (! $list) {
+          return false;
+      }
+
+      $values = collect(json_decode($list->value, true))->mapWithKeys(function ($item, $key) {
+          return [$item['value'] => $item['name']];
+      });
+
+      return $values ;
+    }
 }
