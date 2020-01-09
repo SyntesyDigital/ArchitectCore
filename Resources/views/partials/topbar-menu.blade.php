e948f1b9e5c1628e5193c1177dda65a6b9ded896
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2020-01-09 13:33:10 +0100

    - Now, it's posible to disable a menu item with this on the Modules/{name}/Config/menu.php file :
    [
    	'disabled' => 'medias.index',
    ],

--- Resources/views/partials/topbar-menu.blade.php
+++ Resources/views/partials/topbar-menu.blade.php
@@ -16,11 +16,28 @@
         @endphp
     @endif
 
+    @php 
+        $display = true;
+    @endphp 
+
+    {{-- Is disabled --}}
+    @foreach(config('architect::menu') as $item2)
+        @if(isset($item2['disabled']) && isset($item['route']))
+            @if($item['route'] == $item2['disabled'])
+                @php 
+                    $display = false;
+                @endphp 
+            @endif
+        @endif
+    @endforeach()
+
     {{-- Render the menu item --}}
+    @if(isset($item['route']) && $display)
     <li class="{{ $isActive ? 'active' : false }}">
         <a href="{{ route($item['route']) }}">
             {{ Lang::get($item['label']) }}
         </a>
     </li>
+    @endif
 
 @endforeach()
